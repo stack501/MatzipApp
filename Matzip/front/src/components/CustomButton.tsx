e6ac13d36d05b7af9d16259d0ed7786658966f3f
Junny <stack502@naver.com>
2025-03-26 15:34:10 +0900

    홈 스크린 구현하기

--- Matzip/front/src/components/CustomButton.tsx
+++ Matzip/front/src/components/CustomButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, PressableProps, Dimensions} from 'react-native';
+import {Pressable, StyleSheet, Text, PressableProps, Dimensions, View} from 'react-native';
 import { colors } from '../constants';
 
 interface CustomButtonProps extends PressableProps{
@@ -18,11 +18,12 @@ function CustomButton({label, variant = 'filled', size = 'large', inValid = fals
         disabled={inValid}
         style={({pressed}) => [
             styles.container, 
-            styles[size],
             pressed ? styles[`${variant}Pressed`] : styles[variant],
             inValid && styles.inValid]} 
         {...props}>
-        <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+        <View style={styles[size]}>
+            <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+        </View>
     </Pressable>
   )
 }
@@ -31,6 +32,7 @@ const styles = StyleSheet.create({
     container: {
         borderRadius: 3,
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     inValid: {
         opacity: 0.5,
@@ -55,12 +57,14 @@ const styles = StyleSheet.create({
         paddingVertical: deviceHeight > 700 ? 15 : 10,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     medium: {
         width: '50%',
         paddingVertical: deviceHeight > 700 ? 12 : 8,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     text: {
         fontSize: 16,
--- Matzip/front/src/screens/auth/AuthHomeScreen.tsx
+++ Matzip/front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import {View} from 'react-native';
+import {Dimensions, Image, StyleSheet, View} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigator';
 import { authNavigations } from '../../constants/navigations';
@@ -10,8 +10,15 @@ type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, typeof authNavig
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-        <View>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image 
+            resizeMode="contain"
+            style={styles.image}
+            source={require('../../assets/matzip.png')}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
             <CustomButton 
               label="로그인하기" 
               onPress={() => navigation.navigate(authNavigations.LOGIN)}
@@ -25,5 +32,25 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default AuthHomeScreen;
